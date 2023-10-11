ceeca609ff0ac94f857683ce1c9e65b57ca2bdc4
jacobmoracha@gmail.com <jacobmoracha@gmail.com>
2023-10-11 23:48:10 +0300

    u[pdates

---
+++ src/declarations/token/service.did.d.ts
@@ -0,0 +1,5 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+
+export interface token {}
+export interface _SERVICE extends token {}
---
+++ src/declarations/token/service.did.js
@@ -0,0 +1,5 @@
+export const idlFactory = ({ IDL }) => {
+  const token = IDL.Service({});
+  return token;
+};
+export const init = ({ IDL }) => { return []; };
